729c58970751ce94b115b7bffd5cf0204faaed83
JonCurto <joncurto@gmail.com>
2019-03-30 12:54:07 -0400

    table rows

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,4 +35,40 @@ var mr = $("#mr-input").val()
     mw: mw,
     mr: mr
   })
+  
+})
+
+
+
+database.ref().on("child_added", function(snapshot) {
+
+
+console.log(snapshot.val())
+alert("A new worker has been added!")
+var newEmployee = snapshot.val().employeeName
+var newRole = snapshot.val().role
+var newDate = snapshot.val().startDate
+var newMR = snapshot.val().mr
+var newMW = snapshot.val().mw
+
+console.log(newEmployee)
+console.log(newRole)
+console.log(newDate)
+console.log(newMR)
+console.log(newMW)
+
+var newRow = $("<tr>")
+var newEmp = "<th scope='row'>" + newEmployee + "</th>"
+var newRol = "<td>" + newRole + "</td>"
+var newDat = "<td>" + newDate + "</td>"
+var NMR = "<td>$" + newMR + "</td>"
+var NMW = "<td>" + newMW + "</td>"
+
+newRow.append(newEmp)
+newRow.append(newRol)
+newRow.append(newDat)
+newRow.append(NMW)
+newRow.append(NMR)
+
+$("#table-body").append(newRow)
 })
